7b76bbd7808c495fb6640fdbd3bf44f32479fe34
Jason Hendrickson <jason.hendrickson01@gmail.com>
2014-01-15 11:06:10 -0700

    Add Rec2.py

---
+++ Rec2.py
@@ -0,0 +1,21 @@
+# tuple_of_numbers = (3.14158, 2, 1, -100, 100, 240)
+# 
+# for number in tuple_of_numbers:
+# 	print number
+# 
+# print tuple_of_numbers
+# print tuple_of_numbers[1:3]
+# print tuple_of_numbers[1:-1]
+
+name = 'Mitch'
+
+print name
+print name[0]
+print name[3]
+print name[2:5]
+#print name[2,5]
+for letter in name:
+	print letter
+
+
+	
